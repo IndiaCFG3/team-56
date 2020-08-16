9aff10d16df2ccdd6a3d40183de88be3449936a5
Sonalmercy <60603124+Sonalmercy@users.noreply.github.com>
2020-08-16 13:14:00 +0530

    Update dbconfig.php

--- web-app/dbconfig.php
+++ web-app/dbconfig.php
@@ -1,3 +1,12 @@
 <?php
-$conn=mysqli_connect("localhost","root","","feedback_system")or die(mysqli_error());
+$servername="localhost";
+$dbuser="root";
+$dbpassword="";
+$dbname="code for good team 56";
+
+
+$conn=mysqli_connect($servername,$dbuser,$dbpassword,$dbname);
+if (!$conn) {
+    die('connection failed'.mysqli_connect_error() );
+}
 ?>
